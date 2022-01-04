04acefd9595f0762000ae33265e9ff3a6a586c24
erebuseternal <marcelsanders96@gmail.com>
2022-01-04 09:14:00 -0500

    auth navigator

--- src/navigation/auth.navigator.js
+++ src/navigation/auth.navigator.js
@@ -1,14 +1,19 @@
-import React, { useContext } from 'react';
-import { SafeAreaView, Button } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import { AuthContext } from '../context/auth.context';
+import { TitleScreen } from '../screens/title.screen';
+import { LoginScreen } from '../screens/login.screen';
+
+const Stack = createStackNavigator();
 
 export function AuthNavigator() {
-  const { onLogin } = useContext(AuthContext);
-
   return (
-    <SafeAreaView>
-      <Button title="Login" onPress={onLogin} />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }} gesturesEnabled>
+        <Stack.Screen name="Title" component={TitleScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
